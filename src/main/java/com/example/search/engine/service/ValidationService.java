9c44efa7d3e49f72235cc3752681c6c688b15245
Kacper Kujawa <test@localhost.localdomain>
2022-04-04 16:21:11 +0200

    Implemented more illegal characters handling

--- src/main/java/com/example/search/engine/service/ValidationService.java
+++ src/main/java/com/example/search/engine/service/ValidationService.java
@@ -2,16 +2,23 @@ package com.example.search.engine.service;
 
 import com.example.search.engine.exception.ValidationException;
 import com.example.search.engine.validator.Validator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
+
 public class ValidationService implements Validator {
 
-    public void validate(List<String> words){
-        words.stream()
-                .filter(word -> word.equals("#"))
+    public void validate(List<String> input) {
+        Assert.notNull(input,"input can not be null");
+        String illegalSymbols = "!@#$%^&*()_+-=";
+        input.stream()
+                .filter(word -> StringUtils.containsAny(word, illegalSymbols))
                 .findFirst()
-                .ifPresent(e -> {throw new ValidationException("Illegal Character");});
+                .ifPresent(s -> {
+                    throw new ValidationException("Illegal argument appeared");
+                });
     }
 
 }
