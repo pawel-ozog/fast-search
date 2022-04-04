f08a7cef292809e72263fafb3a5f0351647ae72e
Kacper Kujawa <test@localhost.localdomain>
2022-04-04 16:20:06 +0200

    Added null and empty list tests

--- src/test/java/com/example/search/engine/service/ValidationServiceTest.java
+++ src/test/java/com/example/search/engine/service/ValidationServiceTest.java
@@ -4,23 +4,33 @@ import com.example.search.engine.exception.ValidationException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidationServiceTest {
+    private final ValidationService validationService = new ValidationService();
+
     @Test
     void testValidationException() {
         List<String> words = Arrays.asList("firstWord", "secondWord", "thi@dW&rd");
-        var validationService = new ValidationService();
-        assertThrows(ValidationException.class, () -> validationService.validate(words));
+        assertThrows(ValidationException.class, () -> this.validationService.validate(words));
     }
 
     @Test
     void testProperWord() {
         List<String> words = Arrays.asList("firstWord", "secondWord", "thirdWord");
-        var validationService = new ValidationService();
-        assertDoesNotThrow(() -> validationService.validate(words));
+        assertDoesNotThrow(() -> this.validationService.validate(words));
+    }
+
+    @Test
+    void testInputBeingNull() {
+        assertThrows(IllegalArgumentException.class, () -> this.validationService.validate(null));
+    }
+
+    @Test
+    void testEmptyList(){
+        assertDoesNotThrow(() -> this.validationService.validate(Collections.emptyList()));
     }
 }
