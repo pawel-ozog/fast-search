450fea6e475e5d1c287ba504c6b1d8734f70be7e
Kacper Kujawa <test@localhost.localdomain>
2022-04-06 09:02:23 +0200

    Changed stream on parallelStream

--- src/main/java/com/example/search/engine/service/ValidationService.java
+++ src/main/java/com/example/search/engine/service/ValidationService.java
@@ -1,6 +1,6 @@
 package com.example.search.engine.service;
 
-import com.example.search.engine.exception.ValidationException;
+import com.example.search.engine.exception.IllegalCharacterValidationException;
 import com.example.search.engine.validator.Validator;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.Assert;
@@ -10,14 +10,16 @@ import java.util.List;
 
 public class ValidationService implements Validator {
 
-    public void validate(List<String> input) {
-        Assert.notNull(input,"input can not be null");
-        String illegalSymbols = "!@#$%^&*()_+-=";
-        input.stream()
-                .filter(word -> StringUtils.containsAny(word, illegalSymbols))
+    private static final String ILLEGAL_CHARACTERS = "!@#$%^&*()_+-=";
+
+    public void validate(List<String> inputs) {
+        Assert.notNull(inputs, "inputs can not be null");
+
+        inputs.parallelStream()
+                .filter(word -> StringUtils.containsAny(word, ILLEGAL_CHARACTERS))
                 .findFirst()
                 .ifPresent(s -> {
-                    throw new ValidationException("Illegal argument appeared");
+                    throw new IllegalCharacterValidationException(String.format("Illegal %s character appear", s));
                 });
     }
 
