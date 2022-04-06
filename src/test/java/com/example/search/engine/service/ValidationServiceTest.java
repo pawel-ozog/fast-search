454d0562628164932582afe9fec3bc2f41fb12e5
Kacper Kujawa <test@localhost.localdomain>
2022-04-06 14:23:52 +0200

    Refactored unit test

--- src/test/java/com/example/search/engine/service/ValidationServiceTest.java
+++ src/test/java/com/example/search/engine/service/ValidationServiceTest.java
@@ -2,24 +2,26 @@ package com.example.search.engine.service;
 
 import com.example.search.engine.exception.IllegalCharacterValidationException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidationServiceTest {
     private final ValidationService validationService = new ValidationService();
 
-    @Test
-    void testValidationException() {
-        List<String> first = Arrays.asList("firstWord", "secondWord", "thi@dW&rd");
-        List<String> second = Arrays.asList("firstWord", "s)con!W@r_", "thi@dW&rd");
-        List<String> third = Arrays.asList("$!@#%W()&^%", "s)con!W@r_", "thi@dW&rd");
-        assertThrows(IllegalCharacterValidationException.class, () -> this.validationService.validate(first));
-        assertThrows(IllegalCharacterValidationException.class, () -> this.validationService.validate(second));
-        assertThrows(IllegalCharacterValidationException.class, () -> this.validationService.validate(third));
+    @ParameterizedTest
+    @MethodSource("provideInputLists")
+    void testValidationException(List<String> input) {
+        assertThrows(IllegalCharacterValidationException.class, () -> this.validationService.validate(input));
+        assertThrows(IllegalCharacterValidationException.class, () -> this.validationService.validate(input));
+        assertThrows(IllegalCharacterValidationException.class, () -> this.validationService.validate(input));
     }
 
     @Test
@@ -36,5 +38,13 @@ public class ValidationServiceTest {
     @Test
     void testEmptyList(){
         assertDoesNotThrow(() -> this.validationService.validate(Collections.emptyList()));
+    }
+
+    private static Stream<Arguments> provideInputLists(){
+        return Stream.of(
+                Arguments.of(List.of("firstWord", "secondWord", "thi@dW&rd")),
+                Arguments.of(List.of("firstWord", "s)con!W@r_", "thi@dW&rd")),
+                Arguments.of(List.of("$!@#%W()&^%", "s)con!W@r_", "thi@dW&rd"))
+        );
     }
 }
